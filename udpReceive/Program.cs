197aea5968c8b5e529c6d65921de56c49b9a5560
tomas1978 <tomas.nilsson78@gmail.com>
2022-03-24 16:51:23 +0100

    Simple UDB Examples

---
+++ udpReceive/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace udpReceive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint klientEndPoint = new IPEndPoint(IPAddress.Parse("10.110.212.200"), 0);
+            UdpClient client = new UdpClient(12345);
+            byte[] inström = client.Receive(ref klientEndPoint);
+            string message=Encoding.Unicode.GetString(inström);
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ udpSend/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace udpSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] message=Encoding.Unicode.GetBytes("Hej");
+            IPAddress ip = IPAddress.Parse("10.110.212.200");
+            IPEndPoint destination = new IPEndPoint(ip,12345);
+            UdpClient klient = new UdpClient();
+            klient.Send(message, message.Length, destination);
+        }
+    }
+}
